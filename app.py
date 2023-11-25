9bdca2139937f22403d8e4281712b43e2cb5b5ac
Nanthesh <nanthesh8kannan@gmail.com>
2023-11-25 15:20:46 -0500

    Python

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, request
 import requests
 
-api='a100ec5bbbf4342bd17fc58b72c801ab'
+api='bfe300dcff2eee841916849949ca5992'
 app = Flask(__name__)
 
 @app.route('/')
 def homePage():
     return render_template("index.html")
-@app.route('/weatherapp', method=["POST"])
+
+@app.route('/weatherapp', methods=["POST"])
 def weather_data():
     url1='https://api.openweathermap.org/data/2.5/weather'
 
     param={
         'q':request.form.get("cityName"),
         'units':request.form.get("units"),
-        'appid':request.form.get('apiid')
+        'appid':request.form.get('apiId')
     }
+    print(param)
     res=requests.get(url1,params=param)
     data=res.json()
     return f"Data: {data}"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port =5000)
